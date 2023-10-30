9f51068978f1a3c70a247cae19af53fef091dfb1
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-10-30 22:47:27 +0530

    complete proposal crud

---
+++ src/main/java/org/ucsc/enmoskill/utils/URLFilter.java
@@ -0,0 +1,35 @@
+package org.ucsc.enmoskill.utils;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class URLFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // Cast ServletResponse to HttpServletResponse
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        // Add CORS headers here
+        httpResponse.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500"); // Replace with your frontend origin
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+
+        // Continue with the request chain
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization code, if needed
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup code, if needed
+    }
+}
