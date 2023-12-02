f61fc6732bfb275679c55f82983fdce3242a4ec2
isuruliyanage2001 <isuruliyanageofficial@gmail.com>
2023-12-03 01:07:40 +0530

    Messaging post created

--- src/main/java/org/ucsc/enmoskill/model/MessageModel.java
+++ src/main/java/org/ucsc/enmoskill/model/MessageModel.java
@@ -4,19 +4,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MessageModel {
-    String Message,time,user;
+    String Message,time,user,chatid;
 
-    public MessageModel(String message, String time, String user) {
+    public MessageModel(String message, String time, String user, String chatid) {
         Message = message;
         this.time = time;
         this.user = user;
+        this.chatid = chatid;
     }
+
     public  MessageModel(ResultSet resultSet) throws SQLException {
         this.Message = resultSet.getString("content");
         this.time = resultSet.getString("time");
         this.user = resultSet.getString("receiver_id");
 
     }
+    public String getQuery(String type){
+         if(type.equals("message")){
+
+         }
+        return null;
+    }
+
     public int isYours(String userid){
         if (userid.equals(user)){
             return 1;
@@ -27,11 +36,31 @@ public class MessageModel {
         return Message;
     }
 
+    public void setMessage(String message) {
+        Message = message;
+    }
+
     public String getTime() {
         return time;
     }
 
+    public void setTime(String time) {
+        this.time = time;
+    }
+
     public String getUser() {
         return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getChatid() {
+        return chatid;
+    }
+
+    public void setChatid(String chatid) {
+        this.chatid = chatid;
     }
 }
