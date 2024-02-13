dd02a535c28ff71093186a95d9367d7fa76a82b8
isuruliyanage2001 <isuruliyanageofficial@gmail.com>
2024-02-14 00:41:00 +0530

    validation updated

--- src/main/java/org/ucsc/enmoskill/controller/ValidateUserController.java
+++ src/main/java/org/ucsc/enmoskill/controller/ValidateUserController.java
@@ -1,6 +1,8 @@
 package org.ucsc.enmoskill.controller;
 
+import com.google.gson.JsonObject;
 import org.ucsc.enmoskill.Services.ValidateUserPOST;
+import org.ucsc.enmoskill.utils.TokenService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -49,4 +51,51 @@ public class ValidateUserController extends HttpServlet {
         }
     }
 
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        TokenService tokenService = new TokenService();
+        String token = tokenService.getTokenFromHeader(req);
+
+        if (req.getParameter("check")==null){
+            TokenService.TokenInfo tokenInfo = tokenService.getTokenInfo(token);
+            String Status = tokenService.isTokenValid(token) ? "Valid":"Invalid";
+            String User = tokenInfo.getUserId();
+            String Role = tokenInfo.getRole();
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("Status",Status);
+            jsonObject.addProperty("User",User);
+            jsonObject.addProperty("Role",Role);
+            resp.getWriter().write(jsonObject.toString());
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+
+        } else if(req.getParameter("check").equals("true")){
+
+            if(tokenService.isTokenValid(token)){
+                resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            }else {
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+
+
+        } else if (req.getParameter("check").equals("false")) {
+
+            if(tokenService.isTokenValid(token)){
+
+                TokenService.TokenInfo tokenInfo = tokenService.getTokenInfo(token);
+                String new_token= tokenService.generateToken(tokenInfo.getUserId(),tokenInfo.getRole());
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("JWT",new_token);
+                System.out.println("new_token: "+jsonObject);
+                resp.getWriter().write(jsonObject.toString());
+                resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+
+
+            }else {
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+        }
+
+
+    }
 }
