634e201e54439d621cbed4e319750012260f4f0e
Sajith091” <sajithbandara091@gmail.com>
2024-04-26 10:07:25 +0530

    added authentication for BillingInformationController

--- src/main/java/org/ucsc/enmoskill/controller/BillingInformationController.java
+++ src/main/java/org/ucsc/enmoskill/controller/BillingInformationController.java
@@ -33,17 +33,23 @@ public class BillingInformationController extends HttpServlet {
         if(tokenService.isTokenValidState(token)==1){
             tokenInfo = tokenService.getTokenInfo(token);
 
-            BillingInformationGET service = new BillingInformationGET(tokenInfo);
+            if(tokenInfo.isClient()){
+                BillingInformationGET service = new BillingInformationGET(tokenInfo);
 
-            ResponsModel responsModel = null;
-            try {
-                responsModel = service.Run();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+                ResponsModel responsModel = null;
+                try {
+                    responsModel = service.Run();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+
+                resp.getWriter().write(responsModel.getResMassage());
+                resp.setStatus(responsModel.getResStatus());
+            }else{
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             }
 
-            resp.getWriter().write(responsModel.getResMassage());
-            resp.setStatus(responsModel.getResStatus());
+
         }else if(tokenService.isTokenValidState(token)==2){
             resp.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
         }else{
@@ -59,28 +65,34 @@ public class BillingInformationController extends HttpServlet {
 
         if(tokenService.isTokenValidState(token)==1){
             TokenService.TokenInfo tokenInfo =tokenService.getTokenInfo(token);
-            try (BufferedReader reader = req.getReader()){
 
-                BillingInformationModel billingInformationModel = new Gson().fromJson(reader, BillingInformationModel.class);
+            if(tokenInfo.isClient()){
+                try (BufferedReader reader = req.getReader()){
 
-                if(!billingInformationModel.getFname().isEmpty() && !billingInformationModel.getLname().isEmpty() && !billingInformationModel.getpNumber().isEmpty()&&
-                        !billingInformationModel.getEmail().isEmpty() && !billingInformationModel.getAddress().isEmpty()
-                        && !billingInformationModel.getCity().isEmpty() && !billingInformationModel.getCountry().isEmpty()){
+                    BillingInformationModel billingInformationModel = new Gson().fromJson(reader, BillingInformationModel.class);
 
-                    BillingInformationPOST service = new BillingInformationPOST(billingInformationModel,tokenInfo);
-                    ResponsModel responsModel = service.Run();
-                    resp.getWriter().write(responsModel.getResMassage());
-                    resp.setStatus(responsModel.getResStatus());
+                    if(!billingInformationModel.getFname().isEmpty() && !billingInformationModel.getLname().isEmpty() && !billingInformationModel.getpNumber().isEmpty()&&
+                            !billingInformationModel.getEmail().isEmpty() && !billingInformationModel.getAddress().isEmpty()
+                            && !billingInformationModel.getCity().isEmpty() && !billingInformationModel.getCountry().isEmpty()){
 
-                }else{
-                    resp.getWriter().write("Required field missing");
+                        BillingInformationPOST service = new BillingInformationPOST(billingInformationModel,tokenInfo);
+                        ResponsModel responsModel = service.Run();
+                        resp.getWriter().write(responsModel.getResMassage());
+                        resp.setStatus(responsModel.getResStatus());
+
+                    }else{
+                        resp.getWriter().write("Required field missing");
+                        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    }
+
+                }catch (Exception e) {
+                    resp.getWriter().write(e.toString());
                     resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 }
-
-            }catch (Exception e) {
-            resp.getWriter().write(e.toString());
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }else{
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             }
+
         }else if(tokenService.isTokenValidState(token)==2){
             resp.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
         }else{
@@ -95,28 +107,34 @@ public class BillingInformationController extends HttpServlet {
         if(tokenService.isTokenValidState(token)==1){
 
             TokenService.TokenInfo tokenInfo =tokenService.getTokenInfo(token);
-            try (BufferedReader reader = req.getReader()){
-                BillingInformationModel billingInformationModel = new Gson().fromJson(reader, BillingInformationModel.class);
 
-                if(!billingInformationModel.getFname().isEmpty() && !billingInformationModel.getLname().isEmpty() && !billingInformationModel.getpNumber().isEmpty()&&
-                        !billingInformationModel.getEmail().isEmpty() && !billingInformationModel.getAddress().isEmpty()
-                        && !billingInformationModel.getCity().isEmpty() && !billingInformationModel.getCountry().isEmpty()){
+            if(tokenInfo.isClient()){
+                try (BufferedReader reader = req.getReader()){
+                    BillingInformationModel billingInformationModel = new Gson().fromJson(reader, BillingInformationModel.class);
 
-                    BillingInformationPUT service = new BillingInformationPUT(billingInformationModel,tokenInfo);
-                    ResponsModel responsModel = service.Run();
-                    resp.getWriter().write(responsModel.getResMassage());
-                    resp.setStatus(responsModel.getResStatus());
-                }else{
-                    resp.getWriter().write("Required field missing");
+                    if(!billingInformationModel.getFname().isEmpty() && !billingInformationModel.getLname().isEmpty() && !billingInformationModel.getpNumber().isEmpty()&&
+                            !billingInformationModel.getEmail().isEmpty() && !billingInformationModel.getAddress().isEmpty()
+                            && !billingInformationModel.getCity().isEmpty() && !billingInformationModel.getCountry().isEmpty()){
+
+                        BillingInformationPUT service = new BillingInformationPUT(billingInformationModel,tokenInfo);
+                        ResponsModel responsModel = service.Run();
+                        resp.getWriter().write(responsModel.getResMassage());
+                        resp.setStatus(responsModel.getResStatus());
+                    }else{
+                        resp.getWriter().write("Required field missing");
+                        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    }
+
+
+
+                }catch (Exception e) {
+                    resp.getWriter().write(e.toString());
                     resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 }
-
-
-
-            }catch (Exception e) {
-                resp.getWriter().write(e.toString());
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }else{
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             }
+
         }else if(tokenService.isTokenValidState(token)==2){
             resp.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
         }else{
@@ -132,41 +150,45 @@ public class BillingInformationController extends HttpServlet {
 
         if (tokenService.isTokenValidState(token)==1){
             TokenService.TokenInfo tokenInfo = tokenService.getTokenInfo(token);
-            Connection connection = DatabaseConnection.initializeDatabase();
+
+            if(tokenInfo.isClient()){
+                Connection connection = DatabaseConnection.initializeDatabase();
+                if(connection==null){
+
+                    resp.getWriter().write("SQL Connection Error");
+                    resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+                }else{
+                    if(req.getParameter("update")!=null){
+                        String update =req.getParameter("update");
+
+                        String query = "SELECT t.email,t.contact_no FROM enmo_database.users t WHERE t.userID = "+tokenInfo.getUserId();
+                        try {
+                            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+                            ResultSet result = preparedStatement.executeQuery();
+                            JsonArray jsonArray = new JsonArray();
+                            Gson gson = new Gson();
 
 
-            if(connection==null){
+                            while (result.next()){
+                                BillingInformationModel billingInformationModel = new BillingInformationModel(result,update);
+                                JsonObject jsonObject = gson.toJsonTree(billingInformationModel).getAsJsonObject();
+                                jsonArray.add(jsonObject);
+                            }
 
-            resp.getWriter().write("SQL Connection Error");
-            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-
-            }else{
-                if(req.getParameter("update")!=null){
-                    String update =req.getParameter("update");
-
-                    String query = "SELECT t.email,t.contact_no FROM enmo_database.users t WHERE t.userID = "+tokenInfo.getUserId();
-                    try {
-                        PreparedStatement preparedStatement = connection.prepareStatement(query);
-
-                        ResultSet result = preparedStatement.executeQuery();
-                        JsonArray jsonArray = new JsonArray();
-                        Gson gson = new Gson();
-
-
-                        while (result.next()){
-                            BillingInformationModel billingInformationModel = new BillingInformationModel(result,update);
-                            JsonObject jsonObject = gson.toJsonTree(billingInformationModel).getAsJsonObject();
-                            jsonArray.add(jsonObject);
+                            resp.getWriter().write(jsonArray.toString());
+                            resp.setStatus(HttpServletResponse.SC_OK);
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
                         }
 
-                        resp.getWriter().write(jsonArray.toString());
-                        resp.setStatus(HttpServletResponse.SC_OK);
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e);
                     }
-
                 }
+            }else{
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             }
+
         }else if(tokenService.isTokenValidState(token)==2){
             resp.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
         }else{
